ee390bc8cbf1d8105e65a232aa2c12a6efac3fa5
Laila <00001113934463sp@al.educacao.sp.gov.br>
2024-11-17 13:21:15 -0300

    Flashcards de futebol

---
+++ perguntas.js
@@ -0,0 +1,107 @@
+criaCartao(
+    'Futebol',
+    'Qual o nome do Estádio do RedBull Bragantino?',
+    'Estádio Nabi Abi Chedid'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual time brasileiro faz parte do Grupo City?',
+    'Esporte Clube Bahia'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantos taças da Copa Do Brasil tem o Flamengo?',
+    'Atualmente o Flamengo possuí 5 taças'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o maior vencedor da Copa Libertadores?',
+    ' Independiente de Avellaneda, da Argentina, com 7 conquistas'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o nome do goleiro do Vasco?',
+    'O goleiro do Vasco se chama Léo Jardim'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o artilheiro do Brasileirão Série A?',
+    'Estevão, do Palmeiras'
+)
+
+
+criaCartao(
+    'Futebol',
+    'Qual time conseguiu o acesso para a Série A em 2025?',
+    'Santos'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o último campeão da Libertadores?',
+    'Fluminense'
+)
+
+criaCartao(
+    'Futebol',
+    'Quando foi fundado o Sport Club Corinthians Paulista?',
+    '1 de setembro de 1910'
+)
+
+criaCartao(
+    'Futebol',
+    'Quais são os dois times de futebol campineiros?',
+    'Guarani e Ponte Preta'
+)
+criaCartao(
+    'Futebol',
+    'Qual o mascote do Athletico-PR?',
+    'Furacão ou Fura-cão'
+)
+
+criaCartao(
+    'Futebol',
+    'Quais times são conhecidos pelo clássico Gre-Nal?',
+    'Grêmio e Internacional'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o narrador do Amazon prime Sports?',
+    'Rômulo Mendonça'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas rodadas possuí o campeonato brasileiro?',
+    '38 rodadas'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas Datas FIFA tem por ano?',
+    '5 Datas FIFA (março, junho, setembro, outubro e novembro)'
+)
+
+criaCartao(
+    'Futebol',
+    'De qual cidade é o Grêmio Novorinzontino?',
+    'Novo Horizonte - SP'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o tamanho de um campo de futebol?',
+    'Máximo- 120m / Mínimo- 90m'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o atual técnico da seleção brasileira?',
+    'Dorival Júnior'
+)
